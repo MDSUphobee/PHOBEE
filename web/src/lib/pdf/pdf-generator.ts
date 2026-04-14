322b7a533ef46b5647892442be5b55ddcb718420
tombury59 <tombury59@hotmail.com>
2026-04-14 13:38:54 +0200

    ajustmeent 2 url api env

--- web/src/lib/pdf/pdf-generator.ts
+++ web/src/lib/pdf/pdf-generator.ts
@@ -17,8 +17,8 @@ export async function generatePdf<T>(
   
   // 2. On envoie la requête à l'API PHP
   // On utilise l'URL absolue si on est en dev local, sinon le point d'accès de l'API en prod
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000';
-  
+  const apiUrl = process.env.NEXT_PUBLIC_API_BASE || 'http://127.0.0.1:8000';
+
   const response = await fetch(`${apiUrl}/api/pdfs/fill?pdf=${encodeURIComponent(config.templateName)}&download=${download}`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
