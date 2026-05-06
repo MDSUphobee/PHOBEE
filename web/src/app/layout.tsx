8031b4d168d9d90caf726ec21642f0e6e8db03e1
tombury59 <tombury59@hotmail.com>
2026-05-06 13:51:09 +0200

    GA - intégration du GTM

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -52,7 +52,11 @@ export default function RootLayout({
                     id="gtm-init"
                     strategy="afterInteractive"
                     dangerouslySetInnerHTML={{
-                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GTM_ID}');`,
+                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                            })(window,document,'script','dataLayer','${GTM_ID}');`,
                     }}
                 />
             ) : null}
@@ -66,11 +70,11 @@ export default function RootLayout({
                     />
                     <Script id="ga4-init" strategy="afterInteractive">
                         {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${GA_ID}', { send_page_view: false });
-              `}
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', '${GA_ID}', { send_page_view: false });
+                      `}
                     </Script>
                 </>
             ) : null}
