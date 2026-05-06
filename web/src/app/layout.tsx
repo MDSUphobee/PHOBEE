c9b2fec43421ebd8118f97051b08081c9660f83d
tombury59 <tombury59@hotmail.com>
2026-05-06 12:22:00 +0200

    modif google analytics

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,6 +1,6 @@
 // typescript
 import type { Metadata, Viewport } from "next";
-import { Inter } from "next/font/google"; // Using Inter as requested
+import { Inter } from "next/font/google";
 import { Suspense } from "react";
 import "./globals.css";
 import { cn } from "@/lib/utils";
@@ -39,26 +39,25 @@ export default function RootLayout({
     return (
         <html lang="fr" className="scroll-smooth" suppressHydrationWarning>
         <head>
+            {/* Typekit */}
             <script
                 dangerouslySetInnerHTML={{
                     __html: `(function(d){var config={kitId:'afd8wvd',scriptTimeout:3000,async:true},h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\\bwf-loading\\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)})(document);`,
                 }}
             />
 
+            {/* Google Tag Manager (gtm.js) */}
             {GTM_ID ? (
                 <Script
                     id="gtm-init"
                     strategy="afterInteractive"
                     dangerouslySetInnerHTML={{
-                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${GTM_ID}');`,
+                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GTM_ID}');`,
                     }}
                 />
             ) : null}
 
+            {/* Google Analytics 4 (gtag.js) */}
             {GA_ID ? (
                 <>
                     <Script
@@ -67,16 +66,21 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                     />
                     <Script id="ga4-init" strategy="afterInteractive">
                         {`
-                                window.dataLayer = window.dataLayer || [];
-                                function gtag(){dataLayer.push(arguments);}
-                                gtag('js', new Date());
-                                gtag('config', '${GA_ID}', { send_page_view: false });
-                            `}
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_ID}', { send_page_view: false });
+              `}
                     </Script>
                 </>
             ) : null}
         </head>
-        <body className={cn("min-h-screen bg-background antialiased overflow-x-hidden", inter.variable)} suppressHydrationWarning>
+
+        <body
+            className={cn("min-h-screen bg-background antialiased overflow-x-hidden", inter.variable)}
+            suppressHydrationWarning
+        >
+        {/* GTM noscript for browsers with JS disabled */}
         {GTM_ID ? (
             <noscript
                 dangerouslySetInnerHTML={{
@@ -84,11 +88,14 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 }}
             />
         ) : null}
+
         <ThemeProvider>
             <Suspense fallback={null}>
                 <GaPageView />
             </Suspense>
+
             {children}
+
             <Toaster richColors position="top-center" />
             <CookieBanner />
         </ThemeProvider>
